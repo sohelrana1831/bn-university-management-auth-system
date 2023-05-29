2606d47344de0d61ea43d1e909a53ed122b332d6
sohel rana <sohel.rana@apsissolutions.com>
2023-05-29 23:18:07 +0600

    Project Setup, mongoose, express, typescript, parser, cors

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import config from "./config";
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-        console.log(`🛢 Server is connect successfully`)
+        console.log(`🛢 Server is connect successfully`);
 
         app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-          })
+            console.log(`Application listening on port ${config.port},( http://localhost:5000/ )`);
+        });
     } catch (error) {
-        console.log(`Connection failed`, error)
+        console.log(`Connection failed`, error);
     }
-  }
+}
 
-  main()
+main();
